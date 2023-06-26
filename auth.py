2d3f7d989b34872bec754f7026046a528421fbf3
ASPP Student <student@localhost>
2023-06-26 16:22:31 -0600

    authentication function included

--- auth.py
+++ auth.py
@@ -22,10 +22,26 @@ def add_user(user, password, pwdb):
     if user not in pwdb:
         pwdb[user] = password
     return pwdb
+    
+    
+def authenticate(pwdb): 
+    user, password = get_credentials()
+    if user in pwdb:
+        if pwdb[user] == password:
+           print("Successfully authenticated!")
+        else:
+           print("Wrong password!")
+    else:
+        print("The user does not exist, please:")
+        user, password = get_credentials()
+        add_user(user, password, pwdb)
+    
+    return pwdb
 
 
 if __name__ == '__main__':
     pwdb = read_pwdb()
-    user, password = get_credentials()
-    add_user(user, password, pwdb)
+
+    pwdb = authenticate(pwdb)
+    
     write_pwdb(pwdb)
